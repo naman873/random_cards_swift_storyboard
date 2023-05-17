6ba1afb9e1e534c0420a8bf89aa6dc6782e3a124
namandhiman <namandhiman98@gmail.com>
2023-05-18 01:24:04 +0530

    first push

---
+++ CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,57 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Naman Dhiman on 16/05/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Deck.allCards
+    
+    var timer : Timer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for button in buttons{
+            button.layer.cornerRadius = 8;
+        }
+        startTimer();
+        
+    }
+     
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    
+    func startTimer(){
+    
+        timer =  Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS7")
+    }
+   
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+    }
+    
+  
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
+    }
+    
+    @IBAction func ruleButtonTapped(_ sender: UIButton) {
+    }
+    
+}
